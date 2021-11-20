5de073e7e3f65fdd5521de6add9089220c72fa4d
Prashant Birajdar <92636189+Superprashant@users.noreply.github.com>
2021-11-20 17:31:32 +0530

    Add files via upload

---
+++ using variable two number adiition.cpp
@@ -0,0 +1,15 @@
+/*Write a program in C++ to print the sum of two numbers using variables.
+Print the sum of two numbers
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout << " Enter the value of a :"<<endl;
+cin>> a;
+cout << " Enter the value of b :"<<endl;
+cin>> b;
+cout <<"addition of a+b=" <<a+b;
+return 0;
+}
